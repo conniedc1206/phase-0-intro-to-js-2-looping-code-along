8263a35792ee1f53c101455ea8b337830a88115e
Connie Park <conniedc1206@gmail.com>
2022-05-21 16:52:10 -0700

    commit tree f7b11ddc5e576df487b2978ca0c33a2b42127d77

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+//define a function called writeCards()
+//accepts two arguments: an array of string names and an event name
+//create for loop
+//create a new, empty array to hold the messages;
+    //iterate through the input array and, inside the loop
+    //build out the 'thank you' message for each name using string interpolation
+    //then add that message to the new array you created;
+//after the loop finishes and all of the messages have been added to the new array, return the new array.
+function writeCards(names, event) {
+    const newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+//define a function named countDown
+//parameter: positive integer
+//starting with that number, counts down to zero using console.log()
+
+function countDown(int) {
+    let i = int;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+    return int;
+}
